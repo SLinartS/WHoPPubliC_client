e927db925dfda04c88a9e87a0c8f2a23effee096
SLinartS <dima-akimov-03@mail.ru>
2022-10-14 13:43:14 +0300

    add react-router-dom to the project and make the initial setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import {RouterProvider, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import router from './router';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/router.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Main from './components/Main';
+import ErrorPage from './components/layout/errorPage/ErrorPage';
+import Layout from './components/layout/Layout';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				index: true,
+				element: <Main />,
+			},
+		],
+	},
+]);
+
+export default router;
