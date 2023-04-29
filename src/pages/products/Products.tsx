cda06d22f851ae93281d41039a610427987452f2
SLinartS <dima-akimov-03@mail.ru>
2023-04-30 02:16:10 +0300

    add a selection of displayed products

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -5,24 +5,33 @@ import deleteIcon from '@assets/icons/delete/delete-second.svg';
 import editIcon from '@assets/icons/edit/edit-second.svg';
 import filterIcon from '@assets/icons/sliders/sliders-white.svg';
 import ButtonIcon from '@components/buttonIcon/ButtonIcon';
+import AssembledBlockFieldSelect from '@components/form/assembled/BlockFieldSelect';
 import LoaderWrapper from '@components/loader/wrapper/Wrapper';
 import SearchField from '@components/searchField/SearchField';
 import Table from '@components/table/Table';
 import { useRootStore } from '@helpers/RootStoreProvider/useRootStore';
 import { useFetchOneProductAndFillForm } from '@hooks/product/useFetchOneProductAndFillForm';
 import { useGetSelects } from '@hooks/product/useGetSelects';
+import { IOption } from '@store/category/type';
 import { observer } from 'mobx-react-lite';
-import { FC, MouseEvent, useEffect } from 'react';
+import { FC, MouseEvent, useEffect, useMemo, useState } from 'react';
 
 import { useDeleteController } from '../hooks/delete/useDeleteController';
 import PopupFilter from './popupFilter/PopupFilter';
 
 const Products: FC = () => {
-  const { storeProduct, storePopup, storeTable, storeState, storeAuth } =
-    useRootStore();
+  const {
+    storeProduct,
+    storePopup,
+    storeTable,
+    storeState,
+    storeAuth,
+    storeProductType,
+  } = useRootStore();
   const deleteController = useDeleteController();
   const fetchOneProductAndFillForm = useFetchOneProductAndFillForm();
   const getSelects = useGetSelects();
+  const [selectedProductType, setSelectedProductType] = useState<number>(1);
 
   function addHandler() {
     storePopup.form.state.formActionType = 'create';
@@ -37,6 +46,44 @@ const Products: FC = () => {
     e.stopPropagation();
     storeState.interface.showPopupFilter();
   }
+
+  function changeSelectHandler(option: IOption) {
+    setSelectedProductType(option.id);
+  }
+
+  const currentProductType = useMemo(() => {
+    const id = selectedProductType;
+    const title = storeProductType.state.productTypes.find(
+      (type) => type.id === id,
+    )?.title;
+    if (title) {
+      return { id, title };
+    }
+    return { id, title: '' };
+  }, [selectedProductType, storeProductType.status.get('fetch')]);
+
+  const displayedProductData = useMemo(() => {
+    switch (selectedProductType) {
+      case 1:
+        return storeProduct.state.products.data.filter(
+          (product) => product.typeId.value === 1,
+        );
+      case 2:
+        return storeProduct.state.products.data.filter(
+          (product) => product.typeId.value === 2,
+        );
+      case 3:
+        return storeProduct.state.products.data.filter(
+          (product) => product.typeId.value === 3,
+        );
+      default:
+        return storeProduct.state.products.data;
+    }
+  }, [
+    selectedProductType,
+    storeProductType.status.get('fetch'),
+    storeProduct.status.get('fetch'),
+  ]);
 
   function changeHandler(): void {
     storePopup.form.state.formActionType = 'update';
@@ -60,11 +107,22 @@ const Products: FC = () => {
         storeTable.utils.setDefaultMark(
           'products',
           storeProduct.state.products.data,
-          ['categoryId', 'typeId', 'note', 'printingHouse', 'imageUrl'],
+          [
+            'categoryId',
+            'typeId',
+            'typeAlias',
+            'note',
+            'printingHouse',
+            'imageUrl',
+          ],
         );
       });
     }
   }, [storeProduct.status.get('fetch')]);
+
+  useEffect(() => {
+    storeProductType.action.fetch();
+  }, []);
 
   return (
     <main className='products'>
@@ -104,11 +162,21 @@ const Products: FC = () => {
         />
         <p className='products__filters-text'>Фильтры</p>
       </div>
-
       <div className='products__table'>
+        <div className='products__menu'>
+          <AssembledBlockFieldSelect
+            options={storeProductType.state.productTypes}
+            errors={[]}
+            changeHandler={changeSelectHandler}
+            currentOption={currentProductType}
+            typeForm='product-menu'
+            fieldName='productType'
+            titleText='Тип товара'
+          />
+        </div>
         <LoaderWrapper status={storeProduct.status.get('fetch')}>
           <Table
-            data={storeProduct.state.products.data}
+            data={displayedProductData}
             valuesType='products'
             selectingValues='products'
             displayedColumns={storeTable.utils.getColumnsWithMark('products')}
